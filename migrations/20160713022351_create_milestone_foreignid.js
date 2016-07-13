837ec18c7c8f52d07fea1b2e00961b9bd1f6aac1
sam dunner <samdunner23@gmail.com>
2016-07-13 02:44:42 +0000

    Second migration for foreign ID added

---
+++ migrations/20160713022351_create_milestone_foreignid.js
@@ -0,0 +1,30 @@
+const settings = require("../settings");
+var pg = require('knex')(
+  {client: 'pg',
+  connection: {
+    user:     settings.user,
+    password: settings.password,
+    database: settings.database,
+    host:     settings.hostname,
+    port:     settings.port,
+    ssl:      settings.ssl
+}});
+
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.integer('famous_person_id');
+      table.foreign('famous_person_id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+  knex.schema.table('milestones', function(table){
+    table.dropColumn('famous_person_id');
+    table.foreign(columns, [famous_person_id]);
+    })
+  ])
+};
